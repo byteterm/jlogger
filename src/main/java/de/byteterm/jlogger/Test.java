e9ab1f6b29dc53d077e107162f86bc98f28101c9
Niklas Tat <niklas@byteterm.de>
2022-04-12 17:07:17 +0200

    Test #24g6qa8[SUBMIT-FOR-REVIEW]

--- src/main/java/de/byteterm/jlogger/Test.java
+++ src/main/java/de/byteterm/jlogger/Test.java
@@ -5,6 +5,7 @@ public class Test {
     private static final Logger log = Logger.getLogger();
 
     public static void main(String[] args) {
+        Logger.enableDebug(true);
         log.debug("This is an Logger test...");
     }
 }

1d154d505a579131da262787675f7c1be87376e4
Niklas Tat <niklas.tat@icloud.com>
2022-06-11 21:58:37 +0200

    fix

--- jenkins.groovy
+++ jenkins.groovy
@@ -1,16 +1,16 @@
 def getVersion() {
     gradle_build = readProperties file:"${WORKSPACE}/build.gradle"
-    return gradle_build['version'].trim()
+    return gradle_build['version'].replace("'", "")
 }
 
 def getGroup() {
     gradle_build = readProperties file:"${WORKSPACE}/build.gradle"
-    return gradle_build['group'].trim()
+    return gradle_build['group'].replace("'", "")
 }
 
 def getArtifactId() {
     gradle_properties = readProperties file:"${WORKSPACE}/settings.gradle"
-    return gradle_properties['rootProject.name'].trim()
+    return gradle_properties['rootProject.name'].replace("'", "")
 }
 
 def build() {

52b3e4a5bca9ea2ead3b1651d607dd429ebba9dc
Niklas Tat <2548340-N.Tat@users.noreply.gitlab.com>
2022-08-05 00:12:00 +0200

    New: - Adding a test environment
    Bugs:
    - Fix the problem, that the logger saves only the last line
    - Fix that the logger logs everything in one line
    - Fix that the logger can log bigger elements
    - Fix that the logger overrides the file

---
+++ src/test/java/de/byteterm/jlogger/LoggerTest.java
@@ -0,0 +1,44 @@
+package de.byteterm.jlogger;
+
+import de.byteterm.jlogger.exception.TestException;
+import de.byteterm.jlogger.level.LogLevel;
+import de.byteterm.jlogger.util.ConsoleColor;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.logging.FileHandler;
+
+class LoggerTest {
+
+    private static final Logger log = Logger.getLogger();
+    private final List<String> messageList = Arrays.asList("Lorem ipsum dolor sit amet", "labore et dolore magna", "Excepteur sint occaecat cupidatat non", "sunt in culpa qui officia");
+
+    @Test
+    void testSingleFatalMessage() {
+        log.empty(" ");
+        log.fatal("Single Message test ( Fatal )");
+        log.empty(" ");
+    }
+
+    @Test
+    void testFatalMessageList() {
+        log.empty(" ");
+        log.fatal(messageList);
+        log.empty(" ");
+    }
+
+    @Test
+    void testFatalThrowableMessage() {
+        log.empty(" ");
+        log.fatal(new TestException("This is an generated Exception to test"));
+        log.empty(" ");
+    }
+
+    @Test
+    void testList() {
+        log.empty(" ");
+        log.list(messageList, "Test List", ConsoleColor.CYAN, LogLevel.WARN);
+        log.empty(" ");
+    }
+}
